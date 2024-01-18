ad749ff2f39803939433306b0c71434ba8477448
baekchan1024 <baekchan1024@gmail.com>
2024-01-19 01:09:19 +0900

    [fea] 1.5장, 1.6장 예제 코드

--- src/main/java/chapter01/UserDaoTest.java
+++ src/main/java/chapter01/UserDaoTest.java
@@ -1,7 +1,10 @@
 package chapter01;
 
 import chapter01.connection.DConnectionMaker;
+import chapter01.factory.DaoFactory;
 import chapter01.dao.UserDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -10,6 +13,12 @@ public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         DConnectionMaker dConnectionMaker = new DConnectionMaker();
 
-        UserDao userDao = new DaoFactory().userDao();
+        UserDao userDao1 = new DaoFactory().userDao();
+        UserDao userDao2 = new DaoFactory().userDao();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao1 = ac.getBean("userDao", UserDao.class);
+        UserDao dao2 = ac.getBean("userDao", UserDao.class);
+        System.out.println(userDao1 == userDao2);
+        System.out.println(dao1 == dao2);
     }
 }
--- src/main/java/chapter01/factory/DaoFactory.java
+++ src/main/java/chapter01/factory/DaoFactory.java
@@ -1,13 +1,17 @@
-package chapter01;
+package chapter01.factory;
 
 import chapter01.connection.ConnectionMaker;
 import chapter01.connection.DConnectionMaker;
 import chapter01.dao.AccountDao;
 import chapter01.dao.MessageDao;
 import chapter01.dao.UserDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DaoFactory {
 
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
@@ -20,6 +24,7 @@ public class DaoFactory {
         return new MessageDao(connectionMaker());
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
